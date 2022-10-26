651d0242bcf9917bf1d3b0cb3a7d35332c447dc3
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-26 08:25:41 +0600

    Modify

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function saveData(){
     email=document.getElementById("email").value;
     psw=document.getElementById("psw").value;
 
+//Form data Collect & Store [Array Create]
+    let user_records = new Array();
+
+
+
     localStorage.setItem("name",name);
     localStorage.setItem("email",email);
     localStorage.setItem("psw",psw);
