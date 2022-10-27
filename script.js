dd4179e7f3c0b44fd0de8a981cd0edcef715444b
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-27 08:34:34 +0600

    Modify

--- script.js
+++ script.js
@@ -8,11 +8,22 @@ function saveData(){
     let user_records = new Array();
     user_records = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[];
 
-    user_records.push({
-        "name":name,
-        "email":email,
-        "psw":psw
-    })
-    localStorage.setItem("users",JSON.stringify(user_records));
+
+//Email Velidation
+    if(user_records.some((v)=>{
+        return v.email===email
+    })){
+        alert('Alreadt Registed')
+    }else{
+        user_records.push({
+            "name":name,
+            "email":email,
+            "psw":psw
+        })
+        localStorage.setItem("users",JSON.stringify(user_records));
+    }
+
+
+   
     
 }
